aa3da46b0151fdb256cca3946a966928a883b8d1
diblaze <DeniCevap@gmail.com>
2016-01-28 20:13:37 +0100

    Fixed LunchManager - works perfectly now!

--- uwpPlatenInformationScreen/uwpPlatenInformationScreen/LunchManager.cs
+++ uwpPlatenInformationScreen/uwpPlatenInformationScreen/LunchManager.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using uwpPlatenInformationScreen.Models;
@@ -11,6 +13,10 @@ namespace uwpPlatenInformationScreen
 {
     public static class LunchManager
     {
+        /// <summary>
+        /// Retreives JSON Data from Mashie by parsing the HTML.
+        /// </summary>
+        /// <returns></returns>
         public static async Task<string> GetJsonDataFromMashie()
         {
             const string urlToFetchFrom = @"https://mpi.mashie.eu/public/menu/motala+kommun/af77367d";
@@ -30,16 +36,43 @@ namespace uwpPlatenInformationScreen
             return "Couldn't find Json data from Mashie";
         }
 
-        public static async Task<RootObject> GetTodaysLunch(ObservableCollection<RootObject> menu )
+        /// <summary>
+        /// Retrives JSON data from Mashie and converts it into an object to use.
+        /// </summary>
+        /// <param name="menu">Collection with the lunch menu.</param>
+        /// <returns></returns>
+        public static async Task<RootObject> GetTodaysLunch()
         {
-            string jsonMessage = await GetJsonDataFromMashie();
+            try
+            {
+                string jsonMessage = await GetJsonDataFromMashie();
+                jsonMessage = FixDataTimeJsonData(jsonMessage);
 
-            //deserialize json data into object
-            var serializer = new DataContractJsonSerializer(typeof (RootObject));
-            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+                //deserialize json data into object
+                var serializer = new DataContractJsonSerializer(typeof (RootObject));
+                var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
 
-            RootObject result = (RootObject) serializer.ReadObject(ms);
-            return result;
+                var result = (RootObject) serializer.ReadObject(ms);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                var temp = ex.Message;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Convert JSON message into a readable Microsot JSON message.
+        /// </summary>
+        /// <param name="jsonMessage">JSON data from Mashie.</param>
+        /// <returns></returns>
+        private static string FixDataTimeJsonData(string jsonMessage)
+        {
+            string tempFixed = Regex.Replace(jsonMessage, @"new Date[(]", @"""\/Date(");
+            tempFixed = Regex.Replace(tempFixed, @"d*([)])", @")\/""");
+
+            return tempFixed;
         }
     }
 }
--- uwpPlatenInformationScreen/uwpPlatenInformationScreen/Pages/WelcomePage.xaml.cs
+++ uwpPlatenInformationScreen/uwpPlatenInformationScreen/Pages/WelcomePage.xaml.cs
@@ -1,27 +1,123 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using uwpPlatenInformationScreen.Models;
 
 namespace uwpPlatenInformationScreen.Pages
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class WelcomePage : Page
     {
-        private ObservableCollection<RootObject> lunchMenu; 
+        private static RootObject lunchMenu;
+
         public WelcomePage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
             LoadLunchMenu();
-            lunchMenu = new ObservableCollection<RootObject>();
+        }
+
+        private void PopulateLunchText()
+        {
+            DateTime todayDateTime = DateTime.Today;
+            string nameOfDay = todayDateTime.DayOfWeek.ToString();
+
+            //If computer is set in English, overwrite into Swedish weekdays.
+            switch (nameOfDay)
+            {
+                case "Monday":
+                    nameOfDay = "Måndag";
+                    break;
+                case "Tuesday":
+                    nameOfDay = "Tisdag";
+                    break;
+                case "Wednesday":
+                    nameOfDay = "Onsdag";
+                    break;
+                case "Thursday":
+                    nameOfDay = "Torsdag";
+                    break;
+                case "Friday":
+                    nameOfDay = "Friday";
+                    break;
+                default:
+                    break;
+            }
+
+            if (lunchMenu == null)
+            {
+                return;
+            }
+
+            var stringBuilder = new StringBuilder();
+            List<DayMenu> temp;
+
+            switch (nameOfDay)
+            {
+                case "Måndag":
+                    stringBuilder.AppendLine("Måndag");
+                    temp = lunchMenu.Weeks[0].Days[0].DayMenus;
+                    foreach (DayMenu dayMenu in temp)
+                    {
+                        stringBuilder.Append(dayMenu.MenuAlternativeName + ":");
+                        stringBuilder.AppendLine(dayMenu.DayMenuName);
+                    }
+                    break;
+                case "Tisdag":
+                    stringBuilder.AppendLine("Tisdag");
+                    temp = lunchMenu.Weeks[0].Days[1].DayMenus;
+                    foreach (DayMenu dayMenu in temp)
+                    {
+                        stringBuilder.Append(dayMenu.MenuAlternativeName + ":");
+                        stringBuilder.AppendLine(dayMenu.DayMenuName);
+                    }
+                    break;
+                case "Onsdag":
+                    stringBuilder.AppendLine("Onsdag");
+                    temp = lunchMenu.Weeks[0].Days[2].DayMenus;
+                    foreach (DayMenu dayMenu in temp)
+                    {
+                        stringBuilder.Append(dayMenu.MenuAlternativeName + ":");
+                        stringBuilder.AppendLine(dayMenu.DayMenuName);
+                    }
+                    break;
+                case "Torsdag":
+                    stringBuilder.AppendLine("Tisdag");
+                    temp = lunchMenu.Weeks[0].Days[3].DayMenus;
+                    foreach (DayMenu dayMenu in temp)
+                    {
+                        stringBuilder.Append(dayMenu.MenuAlternativeName + ":");
+                        stringBuilder.AppendLine(dayMenu.DayMenuName);
+                    }
+                    break;
+                case "Fredag":
+                    stringBuilder.AppendLine("Fredag");
+                    temp = lunchMenu.Weeks[0].Days[4].DayMenus;
+                    foreach (DayMenu dayMenu in temp)
+                    {
+                        stringBuilder.Append(dayMenu.MenuAlternativeName + ":");
+                        stringBuilder.AppendLine(dayMenu.DayMenuName);
+                    }
+                    break;
+            }
+
+            string createdString = stringBuilder.ToString();
+            TextTodaysFood.Text = createdString;
+            
+
         }
 
         private async void LoadLunchMenu()
         {
-            //var todaysLunch = await LunchManager.GetTodaysLunch(lunchMenu);
-
+            lunchMenu = new RootObject();
+            lunchMenu = await LunchManager.GetTodaysLunch();
+            PopulateLunchText();
         }
     }
 }
